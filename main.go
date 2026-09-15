319faf85a35a95d6a199757234dd0dca6b5479d5
TURAL-UNI\tural <turalozel@gmail.com>
2026-09-15 13:37:07 +0400

    Added liveness and readiness probes

--- main.go
+++ main.go
@@ -119,6 +119,11 @@ func main() {
 		w.Write([]byte("v1"))
 	})
 
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	fmt.Println("Server starting on :8080")
 	handler := c.Handler(mux)
 	log.Fatal(http.ListenAndServe(":8080", handler))
